ae9e70e53cf8c5f2e066bf499bb1f9040381eb14
IvettaDashkova <ivettazakonno@gmail.com>
2020-10-19 17:26:52 +0300

    Updates

--- modal-buy-now4.8016135f.js
+++ modal-buy-now4.8016135f.js
@@ -175,7 +175,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49588" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54580" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
